81646e4f6b40a63598068c9b74ceba35a0170bc7
Spyke Lionel <social.lionel@gmail.com>
2025-03-19 09:28:56 +0100

    setup test environemt
    Install packages for testing `vitest @vitejs/plugin-react jsdom @testing-library/react @testing-library/jest-dom @testing-library/user-event @types/testing-library__jest-dom`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vitest.config.ts
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Use global APIs (e.g., `describe`, `it`)
+    environment: "jsdom", // Browser-like testing environment
+    setupFiles: "./src/setupTests.ts", // Optional setup file
+    coverage: {
+      provider: "v8", // Use `c8` for coverage
+      reporter: ["text", "json", "html"], // Output formats
+    },
+  },
+});
