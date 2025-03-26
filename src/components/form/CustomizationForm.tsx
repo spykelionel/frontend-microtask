fafe9eec60c7820ad5bcf134cf4393c31c0ce49b
Spyke Lionel <social.lionel@gmail.com>
2025-03-26 12:55:59 +0100

    refactor code:
    Make adjustments to the customization form. Remove opacity as no longer needed

--- src/components/form/CustomizationForm.tsx
+++ src/components/form/CustomizationForm.tsx
@@ -1,13 +1,10 @@
-import { Sliders } from "lucide-react";
 import React from "react";
-import useBannerContext from "../../hooks/useBannerContext";
 import ActionButtons from "./form-sections/ActionButtons";
 import BackgroundColorControl from "./form-sections/BackgroundColorControl";
 import TextControls from "./form-sections/TextControls";
 import TextStyleControls from "./form-sections/TextStyleControls";
 
 const CustomizationForm: React.FC = () => {
-  const { settings, updateSettings } = useBannerContext();
   return (
     <div
       className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-8 mb-8"
@@ -16,26 +13,7 @@ const CustomizationForm: React.FC = () => {
       <h2 className="text-2xl font-bold mb-6">Customize Your Banner</h2>
 
       <div className="grid grid-cols-1">
-        <div className="">
-          {/* <BannerStyleSelector /> */}
-          <label className="text-sm font-medium text-gray-700 mb-2 flex items-center">
-            <Sliders size={18} className="mr-2 text-blue-500" />
-            Overlay Opacity: {settings.overlayOpacity}
-          </label>
-          <input
-            type="range"
-            min="0"
-            max="0.9"
-            step="0.1"
-            value={settings.overlayOpacity}
-            onChange={(e) =>
-              updateSettings({ overlayOpacity: parseFloat(e.target.value) })
-            }
-            className="w-full accent-blue-500"
-            data-testid="opacity-slider"
-          />
-          <BackgroundColorControl />
-        </div>
+        <BackgroundColorControl />
 
         <div className="md:col-span-2">
           <TextStyleControls />
